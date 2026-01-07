56bebeeb081ffbf45b756bd7c36393d6f419a41f
vovanmai <vovanmai.dt3@gmail.com>
2026-01-07 09:25:36 +0700

    edit time booking

--- resources/views/user/make_reservation.blade.php
+++ resources/views/user/make_reservation.blade.php
@@ -153,25 +153,9 @@
                                     <div class="form-group col-md-6 mb-5">
                                         <select name="time" class="form-control form-select" id="time">
                                             <option value="" disabled selected>Time (*)</option>
-                                            <option value="09:00 AM">09:00 AM</option>
-                                            <option value="09:30 AM">09:30 AM</option>
-                                            <option value="10:00 AM">10:00 AM</option>
-                                            <option value="10:30 AM">10:30 AM</option>
-                                            <option value="11:00 AM">11:00 AM</option>
-                                            <option value="11:30 AM">11:30 AM</option>
-                                            <option value="12:00 PM">12:00 PM</option>
-                                            <option value="12:30 PM">12:30 PM</option>
-                                            <option value="01:00 PM">01:00 PM</option>
-                                            <option value="01:30 PM">01:30 PM</option>
-                                            <option value="02:00 PM">02:00 PM</option>
-                                            <option value="02:30 PM">02:30 PM</option>
-                                            <option value="03:00 PM">03:00 PM</option>
-                                            <option value="03:30 PM">03:30 PM</option>
-                                            <option value="04:00 PM">04:00 PM</option>
-                                            <option value="04:30 PM">04:30 PM</option>
-                                            <option value="05:00 PM">05:00 PM</option>
-                                            <option value="05:30 PM">05:30 PM</option>
-                                            <option value="06:00 PM">06:00 PM</option>
+                                            <option value="09:00">09:00</option>
+                                            <option value="12:00">12:00</option>
+                                            <option value="15:00">15:00</option>
                                         </select>
                                         <div class="help-block with-errors"></div>
                                     </div>
